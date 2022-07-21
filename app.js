37f2ea8a3dabbccc8c59a85c6d5fee08c16f6fa1
Meakara Peou <peou_meakara@hotmail.com>
2022-07-21 06:48:27 +0200

    users creation

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require ('mongoose');
+
+//const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://mpeou08:Meakara1977@cluster0.vdkcm.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((error) => console.log(error));
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Requête reçue!');
-    next();
-});
-
-app.use((req, res, next => {
-    res.status(201);
-    next();
-}))
+app.use(express.json());
 
 app.use((req, res, next) => {
-    res.json({ message: 'votre requête a bien été reçue'});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-})
+app.use(bodyParser.json());
+
+//app.use('/api/stuff', stuffRoutes);
+app.use('/api', userRoutes);
+
 
 module.exports = app;
+
---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const auth = require('../middleware/auth');
+const router = express.Router();
+
+const userCtrl = require('../controllers/user');
+
+//router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/auth/signup', userCtrl.signup);
+router.post('/auth/login', userCtrl.login);
+
+module.exports = router;
